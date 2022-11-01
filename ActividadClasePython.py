d8268e0bcc2644ef3a5aefbdb12f12113322d39d
thiago1251 <115585014+thiago1251@users.noreply.github.com>
2022-11-01 14:10:24 -0500

    Santiago Tapia

---
+++ ActividadClasePython.py
@@ -0,0 +1,33 @@
+Espacio = ['----------------------------------------------']
+
+paises = ['Malacia','India','Ibiza', 'Paris','Santorini']
+print (paises)
+print (Espacio)
+
+print (sorted(paises))
+print (Espacio)
+
+print (paises)
+print (Espacio)
+
+x = sorted(paises, reverse = True)
+print (x)
+print (Espacio)
+
+print (paises)
+print (Espacio)
+
+paises.reverse()
+print (paises)
+print (Espacio)
+
+paises.reverse()
+print (paises)
+print (Espacio)
+
+paises.sort()
+print (paises)
+print (Espacio)
+
+paises.sort(reverse=True)
+print(paises)
